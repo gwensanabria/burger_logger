d676cfe0e2193e72a6e7f63af0ce857cb4ca3c7a
Gwendoline Sanabria <gwensanabria@gmail.com>
2020-07-25 23:49:08 -0500

    jawsdb

--- config/connection.js
+++ config/connection.js
@@ -2,13 +2,19 @@
 const mysql = require('mysql')
 require('dotenv').config();
 
-const connection = mysql.createConnection({
+const connection; 
+
+if(process.env.JAWSDB_URL) {
+    connection = mysql.createConnection(process.env.JAWSDB_URL);
+} else {
+    connection = mysql.createConnection({
     host: 'localhost',
     port: 3306,
     user: 'root',
     password: process.env.PASSWORD,
     database: 'burger_db'
 })
+}
 
 // make connection
 connection.connect(function(err) {
